5c3db64b5f6e5091f50c0a3a81e558ccea04c9d3
dennyh89 <denny.herbrich@gmail.com>
2016-09-12 16:33:46 +0200

    initial commit for local running Service registry

---
+++ src/main/java/com/spring/cloud/ref/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.spring.cloud.ref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+@EnableAutoConfiguration
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
